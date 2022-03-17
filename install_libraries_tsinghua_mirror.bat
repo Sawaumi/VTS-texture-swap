956aa84fcc704fa357f82eeadf9bf9b1a4b04cc9
Shamil <sawaumi@foxmail.com>
2022-03-17 12:26:09 +0800

    Update and rename 运行库补全（镜像源）.bat to install_libraries_tsinghua_mirror.bat

---
+++ install_libraries_tsinghua_mirror.bat
@@ -0,0 +1,3 @@
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+@echo  installing libraries from tsinghua mirror finished (if theres no large chuncks of error messages above)
+@pause
