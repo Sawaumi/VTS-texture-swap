7b04ee1ab6f281acc93c5621450526142e3b9034
Shamil <sawaumi@foxmail.com>
2022-03-18 12:02:55 +0800

    Update to 0.2.0, auto set up and more
    Added auto texture path set up, muti texture swap support, fade in/out on lost face tracking, and a non-functional trigger animation (which toggles my model's hair)

--- VtextureSwap.py
+++ VtextureSwap.py
@@ -1,58 +1,96 @@
-import json
-import time
-import os
-
 import setup
 from setup import *
+import websockets
+from func import *
+from util import *
 
-from nameSwap import swap
 
-###########################################
-#          texture path settings          #
-###########################################
-# Please replace content in <> 
-# with the absolute path for the texture folder in your model directory
-# e.g:
-# path = r'G:\SteamLibrary\steamapps\common\VTube Studio\VTube Studio_Data\StreamingAssets\Live2DModels\juzi_1123\juzi.4096'
-path = r'<Absolute path for the texture folder in your model directory>'
-
-# default = 'texture_00.png'
-src_file_name = 'texture_00.png'
-
-# default = 'texture_00_swap.png'
-dst_file_name = 'texture_00_swap.png'
-
-# Swap is done by src->temp, dst->src, temp->dst
-# default = 'texture_00_temp.png'
-temp_file_name = 'texture_00_temp.png'
-# ======================================
+# from nameSwap import swap
 
 
 async def main():
+    # async with websockets.connect('ws://127.0.0.1:8001') as ws:
     try:
         ws = await websockets.connect('ws://127.0.0.1:8001')
-    except:
-        print("Couldn't connect to vtube studio")
+    except Exception as e:
+        print("Couldn't connect to VtubeStudio")
+        print(e)
         input("press enter to quit program")
         quit()
     await setup(ws)
 
-    abs_path = path.replace('\\', '/') + '/'
+    with open('textures.json', "r") as json_file:
+        data = json.load(json_file)
+        base_path = data["basePath"]
+        textures = data["textureArray"]
+        src_path, dst_path, temp_path = [], [], []
+        for texture in textures:
+            src_path.append(base_path + texture)
+            dst_path.append(base_path + texture.replace('.png', '_swap.png'))
+            temp_path.append(base_path + texture.replace('.png', '_temp.png'))
+        json_file.close()
 
-    src_file = abs_path + src_file_name
-    temp_file = abs_path + temp_file_name
-    dst_file = abs_path + dst_file_name
+    # abs_path = path.replace('\\', '/') + '/'
+    #
+    # src_file = abs_path + src_file_name
+    # temp_file = abs_path + temp_file_name
+    # dst_file = abs_path + dst_file_name
+
+    # task_idle_trigger = asyncio.create_task(idle_sequence(ws))
+    # task_remove_hair_trigger = asyncio.create_task(remove_hair_sequence(ws))
+    # task_texture_swap_trigger = asyncio.create_task(texture_swap_sequence(ws, src_file, temp_file, dst_file))
+    # 
+    # await task_idle_trigger
+    # await task_remove_hair_trigger
+    # await task_texture_swap_trigger
+    # 
+    # await asyncio.gather(idle_sequence(ws),
+    #                      remove_hair_sequence(ws),texture_swap_sequence(ws, src_file, temp_file, dst_file))
+
+    # print(await get_model_art_mesh_list(ws))
+
+    await exec_hotkey(ws, "ResetAllExpressions")
+    swapped = await if_exp_active(ws, "textureSwap.exp3.json")
+    texture_idled = True
+    hair_lost = False
 
     while True:
-        trigger = await get_exp_state(ws, "textureSwap.exp3.json", True)
-        trigger = trigger["data"]["expressions"][0]["active"]
-        while trigger:
-            print("triggered")
-            swap(src_file, temp_file, dst_file)
-            trigger = False
-            time.sleep(1.3)
-            await exec_hotkey(ws, "ReloadModelTexture")
-        time.sleep(0.03)
+        # Swap texture
+        swap_trigger = await if_exp_active(ws, "textureSwap.exp3.json")
+        if swap_trigger ^ swapped:
+            await swap_texture(ws, src_path, dst_path, temp_path)
+            swapped ^= True
+
+        # idle animation
+        face_is_found = await if_face_found(ws)
+        # Uncomment next line to bypass this feature
+        face_is_found = True
+        texture_idled = await idle_texture(ws, face_is_found, texture_idled)
+        # if face_is_found and not in_idle:
+        #     print("back from idle")
+        #     await fade_back(ws, if_tint_all=True)
+        #     in_idle = True
+        #
+        # elif not face_is_found and in_idle:
+        #     print("gone idle")
+        #     await fade_away(ws, if_tint_all=True)
+        #     in_idle = False
+
+        # Toggle hair
+        hair_loss_triggered = await if_exp_active(ws, "removeHair.exp3.json")
+        hair_lost = await hair_loss(ws, hair_loss_triggered, hair_lost)
+        # if hair_loss_triggered and not hair_loss_toggle:
+        #     print("removed hair")
+        #     await fade_away(ws, con_array=["ce", "fa", "qian", "hfa", "tding", "ying"])
+        #     hair_loss_toggle = True
+        #
+        # elif not hair_loss_triggered and hair_loss_toggle:
+        #     print("added hair")
+        #     await fade_back(ws, con_array=["ce", "fa", "qian", "hfa", "tding", "ying"])
+        #     hair_loss_toggle = False
+
+        # bottom of main loop
+        await asyncio.sleep(0.03)
 
 
 asyncio.run(main())
