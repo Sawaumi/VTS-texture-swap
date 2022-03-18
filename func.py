7e2c3a85f8579a580495eb2eddca749287885980
Shamil <sawaumi@foxmail.com>
2022-03-18 12:06:26 +0800

    Update to 0.2.0, auto set up and more
    Added auto texture path set up, muti texture swap support, fade in/out on lost face tracking, and a non-functional trigger animation (which toggles my model's hair)
    most VTS related functions were moved here

--- func.py
+++ func.py
@@ -1,12 +1,14 @@
+import asyncio
 from os import system, name
 import json
-import time
+
+from util import *
 
 ######################################
 #          plugin settings           #
 ######################################
 dev = "Sawaumi"
-request_id = "VtextureSwap_v0.1.1 "
+request_id = "VtextureSwap_v0.2.0 "
 plugin_name = "VtextureSwap"
 api_version = "1.0"
 
@@ -182,6 +184,24 @@ async def get_states(ws):
     return pack
 
 
+async def if_face_found(ws):
+    payload = {
+        "apiName": "VTubeStudioPublicAPI",
+        "apiVersion": "1.0",
+        "requestID": "SomeID",
+        "messageType": "FaceFoundRequest"
+    }
+    await ws.send(json.dumps(payload))
+    json_data = await ws.recv()
+    pack = json.loads(json_data)
+    try:
+        data = pack["data"]["found"]
+        return data
+    except KeyError:
+        print(pack)
+    # return pack["data"]["found"]
+
+
 async def get_vts_folder_info(ws):
     payload = {
         "apiName": "VTubeStudioPublicAPI",
@@ -243,11 +263,11 @@ async def exec_hotkey(ws, hotkey_id):
 #     return
 
 
-async def get_exp_state(ws, exp_file_name, if_include_detail, ):
+async def get_exp_state(ws, exp_file_name, if_include_detail=False):
     payload = {
         "apiName": "VTubeStudioPublicAPI",
         "apiVersion": api_version,
-        "requestID": "SomeID",
+        "requestID": request_id,
         "messageType": "ExpressionStateRequest",
         "data": {
             "details": if_include_detail,
@@ -258,6 +278,29 @@ async def get_exp_state(ws, exp_file_name, if_include_detail, ):
     json_data = await ws.recv()
     pack = json.loads(json_data)
     return pack
+
+
+async def if_exp_active(ws, exp_file_name):
+    payload = {
+        "apiName": "VTubeStudioPublicAPI",
+        "apiVersion": api_version,
+        "requestID": request_id,
+        "messageType": "ExpressionStateRequest",
+        "data": {
+            "details": False,
+            "expressionFile": exp_file_name,
+        }
+    }
+    await ws.send(json.dumps(payload))
+    # logging.info("calling recv", exc_info=True)
+    json_data = await ws.recv()
+    pack = json.loads(json_data)
+    try:
+        data = pack["data"]["expressions"][0]["active"]
+        return data
+    except KeyError:
+        print(pack)
+    # return pack["data"]["expressions"][0]["active"]
 
 
 async def load_model(ws, model_id):
@@ -297,7 +340,7 @@ async def move_model(ws, t, relativity, x, y, rot, size):
     return pack
 
 
-async def get_art_mesh(ws):
+async def get_model_art_mesh_list(ws):
     payload = {
         "apiName": "VTubeStudioPublicAPI",
         "apiVersion": api_version,
@@ -310,7 +353,12 @@ async def get_art_mesh(ws):
     return pack
 
 
-async def tint_art_mesh(ws, r, g, b, a, tint_all, num, exact_array, con_array, tag_exact_array, tag_contain_array):
+async def tint_art_mesh(ws, r=255, g=255, b=255, a=255,
+                        if_tint_all=False, enable_rainbow=False,
+                        if_mix_with_scene_lighting=True,
+                        art_mesh_number="",
+                        exact_array="", con_array="",
+                        tag_exact_array="", tag_contain_array=""):
     payload = {
         "apiName": "VTubeStudioPublicAPI",
         "apiVersion": api_version,
@@ -321,11 +369,13 @@ async def tint_art_mesh(ws, r, g, b, a, tint_all, num, exact_array, con_array, t
                 "colorR": r,
                 "colorG": g,
                 "colorB": b,
-                "colorA": a
+                "colorA": a,
+                "mixWithSceneLightingColor": if_mix_with_scene_lighting,
+                "jeb_": enable_rainbow
             },
             "artMeshMatcher": {
-                "tintAll": tint_all,
-                "artMeshNumber": num,
+                "tintAll": if_tint_all,
+                "artMeshNumber": art_mesh_number,
                 "nameExact": exact_array,
                 "nameContains": con_array,
                 "tagExact": tag_exact_array,
@@ -341,28 +391,70 @@ async def tint_art_mesh(ws, r, g, b, a, tint_all, num, exact_array, con_array, t
 
 async def spin(ws, x, y, s):
     await move_model(ws, 0.2, False, x, y, 90, s)
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
     await move_model(ws, 0.2, False, x, y, 180, s)
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
     await move_model(ws, 0.2, False, x, y, 270, s)
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
     await move_model(ws, 0.2, False, x, y, 360, s)
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
 
 
-async def rainbow(ws):
-    await tint_art_mesh(ws, 255, 0, 0, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 255, 127, 0, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 255, 255, 0, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 0, 255, 0, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 0, 0, 255, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 46, 43, 95, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 139, 0, 255, 255, True, "", "", "", "", "")
-    time.sleep(0.03)
-    await tint_art_mesh(ws, 255, 255, 255, 255, True, "", "", "", "", "")
+async def fade_away(ws, if_tint_all=False, con_array=None):
+    if con_array is None:
+        con_array = [""]
+    for i in range(16):
+        await tint_art_mesh(ws, a=255 - i * 17, if_tint_all=if_tint_all, con_array=con_array)
+        await asyncio.sleep(0.02)
+    # print("faded away")
+    # print(con_array)
+
+
+async def fade_back(ws, if_tint_all=False, con_array=None):
+    if con_array is None:
+        con_array = [""]
+    for i in range(16):
+        await tint_art_mesh(ws, a=i * 17, if_tint_all=if_tint_all, con_array=con_array)
+        await asyncio.sleep(0.02)
+    # print("faded back")
+    # print(con_array)
+
+
+async def swap_texture(ws, src_path, dst_path, temp_path):
+    for src, dst, temp in zip(src_path, dst_path, temp_path):
+        swap(src, dst, temp)
+    await asyncio.sleep(2)
+    await exec_hotkey(ws, "ReloadModelTexture")
+    print("texture_swapped")
+
+
+async def swap_debug(ws, src_path, dst_path, temp_path):
+    for src, dst, temp in zip(src_path, dst_path, temp_path):
+        print(src, dst, temp)
+
+
+async def idle_texture(ws, face_is_found, texture_idled):
+    if face_is_found and not texture_idled:
+        print("back from idle")
+        await fade_back(ws, if_tint_all=True)
+        texture_idled = True
+
+    elif not face_is_found and texture_idled:
+        print("gone idle")
+        await fade_away(ws, if_tint_all=True)
+        texture_idled = False
+    return texture_idled
+
+
+async def hair_loss(ws, hair_loss_triggered, hair_loss_toggle):
+    if hair_loss_triggered and not hair_loss_toggle:
+        print("removed hair")
+        await fade_away(ws, con_array=["ce", "fa", "qian", "hfa", "tding", "ying"])
+        hair_loss_toggle = True
+
+    elif not hair_loss_triggered and hair_loss_toggle:
+        print("added hair")
+        await fade_back(ws, con_array=["ce", "fa", "qian", "hfa", "tding", "ying"])
+        hair_loss_toggle = False
+    return hair_loss_toggle
+
