510a5d30a527b61508e6c213430fd3f37ee2f35b
Shamil <sawaumi@foxmail.com>
2022-03-19 13:47:02 +0800

    Delete install_libraries_tsinghua_mirror.bat
    0.2.0 update: packaged as .exe, bat deprecated

--- install_libraries_tsinghua_mirror.bat
+++
@@ -1,3 +0,0 @@
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-@echo  installing libraries from tsinghua mirror finished (if theres no large chuncks of error messages above)
-@pause
